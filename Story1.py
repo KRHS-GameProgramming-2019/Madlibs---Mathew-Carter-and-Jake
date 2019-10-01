42509844564e9721946ec6e66202ba6aea558c3c
mathew churchwell <matthew.churchwell@kearsarge.org>
2019-10-01 11:25:20 -0400

    end october 1

--- Story1.py
+++ Story1.py
@@ -4,13 +4,16 @@ def Story1(debug = False):
     if debug: print("Story1 Function")
 
     print("\n")
-    friendName1 = getName("Enter a name: ", debug)
-    sport1 = getSport("Enter a Sport: ", debug)
-    Noun1 = getNoun("Enter a Noun") 
+    Name1 = getWord("Enter a name: ", debug)
+    sport1 = getWord("Enter a Sport: ", debug)
+    Noun1 = getWord("Enter a Noun") 
+    Noun2 = getWord("Enter another Noun")
     out = "\n"
     out += "One day me and my friend, " + Name1
     out += " were out playing " + sport1
     out +="then we saw a " + Noun1
     out +=" it was very " + ad1
-
+    out +=" then my favorite " + Noun2 + "walked out"
     return out
+
+print(Story1())
